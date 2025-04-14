df85109a31da4495e4c497455cc93b2b628c155e
DELECHENEAU Camille <delecheneaucamille@gmail.com>
2025-04-14 14:56:11 +0200

    menu a retravailler

---
+++ sources/menu.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "struct.h"
+#define MIN -5
+#define MAX 15
+
+typedef enum
+{
+    PLAY_VS_AI = 1,
+    PLAY_VS_PLAYER,
+    LOAD_GAME,
+    SHOW_RULES,
+    EXIT
+}
+
+int
+choiceGame()
+{
+    int choice;
+    printf("Press '1' play vs AI\nPress '2' play vs player\nPress '3' to load a game\nPress '4' to see the rules\nPress '5' to exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+void selectGameMode()
+{
+    int choice;
+    do
+    {
+        choice = choiceGame();
+        switch (choice)
+        {
+        case PLAY_VS_AI:
+            printf("You chose to play against AI.\n");
+            break;
+        case PLAY_VS_PLAYER:
+            printf("You chose to play against another player.\n");
+            break;
+        case LOAD_GAME:
+            printf("You chose to load a game.\n");
+            break;
+        case SHOW_RULES:
+            printf("You chose to see the rules.\n");
+            break;
+        case EXIT:
+            printf("Exiting the game.\n");
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+            choice = 0; // Reset choice to continue the loop
+        }
+    } while (choice == 0);
+}
+
+void selectValuesCard(int *pmin, int *pmax)
+{
+
+    int min, max;
+    printf("Enter the minimum value of the card: ");
+    scanf("%d", &min);
+    printf("Enter the maximum value of the card: ");
+    scanf("%d", &max);
+
+    if (min < MIN || max < MAX || min > max)
+    {
+        printf("Invalid values. Please enter positive values with min <= max.\n");
+        selectValuesCard(pmin, pmax);
+        return;
+    }
+
+    *pmin = min;
+    *pmax = max;
+}
+int main()
+{
+
+    return 0;
+}
